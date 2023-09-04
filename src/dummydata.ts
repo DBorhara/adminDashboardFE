02237ce04a78b34ff3bc0b90a1f371a26068ac0d
Depak Borhara <depakborhara@gmail.com>
2023-09-04 12:57:11 -0400

    Dummy data file

---
+++ src/dummydata.ts
@@ -0,0 +1,179 @@
+export const userData = [
+  {
+    month: "Jan",
+    "Active Users": 4000,
+  },
+  {
+    month: "Feb",
+    "Active Users": 5000,
+  },
+  {
+    month: "Mar",
+    "Active Users": 6000,
+  },
+  {
+    month: "Apr",
+    "Active Users": 2000,
+  },
+  {
+    month: "May",
+    "Active Users": 3000,
+  },
+  {
+    month: "June",
+    "Active Users": 4000,
+  },
+  {
+    month: "July",
+    "Active Users": 5000,
+  },
+  {
+    month: "Aug",
+    "Active Users": 6000,
+  },
+  {
+    month: "Sept",
+    "Active Users": 3000,
+  },
+  {
+    month: "Oct",
+    "Active Users": 5000,
+  },
+  {
+    month: "Nov",
+    "Active Users": 4000,
+  },
+  {
+    month: "Dec",
+    "Active Users": 7000,
+  },
+];
+
+export const userRows = [
+  {
+    id: 1,
+    userName: "JSnow",
+    avatar:
+      "https://i.insider.com/5d03bccc6fc92000a22eadc9?width=1000&format=jpeg&auto=webp",
+    email: "jon@thenorth.com",
+    status: "active",
+    transaction: "$120.00",
+  },
+  {
+    id: 2,
+    userName: "JSnow",
+    avatar:
+      "https://i.insider.com/5d03bccc6fc92000a22eadc9?width=1000&format=jpeg&auto=webp",
+    email: "jon@thenorth.com",
+    status: "active",
+    transaction: "$120.00",
+  },
+  {
+    id: 3,
+    userName: "JSnow",
+    avatar:
+      "https://i.insider.com/5d03bccc6fc92000a22eadc9?width=1000&format=jpeg&auto=webp",
+    email: "jon@thenorth.com",
+    status: "active",
+    transaction: "$120.00",
+  },
+  {
+    id: 4,
+    userName: "JSnow",
+    avatar:
+      "https://i.insider.com/5d03bccc6fc92000a22eadc9?width=1000&format=jpeg&auto=webp",
+    email: "jon@thenorth.com",
+    status: "active",
+    transaction: "$120.00",
+  },
+  {
+    id: 5,
+    userName: "JSnow",
+    avatar:
+      "https://i.insider.com/5d03bccc6fc92000a22eadc9?width=1000&format=jpeg&auto=webp",
+    email: "jon@thenorth.com",
+    status: "active",
+    transaction: "$120.00",
+  },
+  {
+    id: 6,
+    userName: "JSnow",
+    avatar:
+      "https://i.insider.com/5d03bccc6fc92000a22eadc9?width=1000&format=jpeg&auto=webp",
+    email: "jon@thenorth.com",
+    status: "active",
+    transaction: "$120.00",
+  },
+  {
+    id: 7,
+    userName: "JSnow",
+    avatar:
+      "https://i.insider.com/5d03bccc6fc92000a22eadc9?width=1000&format=jpeg&auto=webp",
+    email: "jon@thenorth.com",
+    status: "active",
+    transaction: "$120.00",
+  },
+  {
+    id: 8,
+    userName: "JSnow",
+    avatar:
+      "https://i.insider.com/5d03bccc6fc92000a22eadc9?width=1000&format=jpeg&auto=webp",
+    email: "jon@thenorth.com",
+    status: "active",
+    transaction: "$120.00",
+  },
+  {
+    id: 9,
+    userName: "JSnow",
+    avatar:
+      "https://i.insider.com/5d03bccc6fc92000a22eadc9?width=1000&format=jpeg&auto=webp",
+    email: "jon@thenorth.com",
+    status: "active",
+    transaction: "$120.00",
+  },
+  {
+    id: 10,
+    userName: "JSnow",
+    avatar:
+      "https://i.insider.com/5d03bccc6fc92000a22eadc9?width=1000&format=jpeg&auto=webp",
+    email: "jon@thenorth.com",
+    status: "active",
+    transaction: "$120.00",
+  },
+  {
+    id: 11,
+    userName: "JSnow",
+    avatar:
+      "https://i.insider.com/5d03bccc6fc92000a22eadc9?width=1000&format=jpeg&auto=webp",
+    email: "jon@thenorth.com",
+    status: "active",
+    transaction: "$120.00",
+  },
+  {
+    id: 12,
+    userName: "JSnow",
+    avatar:
+      "https://i.insider.com/5d03bccc6fc92000a22eadc9?width=1000&format=jpeg&auto=webp",
+    email: "jon@thenorth.com",
+    status: "active",
+    transaction: "$120.00",
+  },
+  {
+    id: 13,
+    userName: "JSnow",
+    avatar:
+      "https://i.insider.com/5d03bccc6fc92000a22eadc9?width=1000&format=jpeg&auto=webp",
+    email: "jon@thenorth.com",
+    status: "active",
+    transaction: "$120.00",
+  },
+  {
+    id: 14,
+    userName: "JSnow",
+    avatar:
+      "https://i.insider.com/5d03bccc6fc92000a22eadc9?width=1000&format=jpeg&auto=webp",
+    email: "jon@thenorth.com",
+    status: "active",
+    transaction: "$120.00",
+  },
+];
