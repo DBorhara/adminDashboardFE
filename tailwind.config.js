d40635e3f68581c7c38922e07d4104f50be67e00
Depak Borhara <depakborhara@gmail.com>
2023-09-04 12:55:59 -0400

    Tailwind added, packages added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,15 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      boxShadow: {
+        featuredInfoBoxShadow: "0px 0px 15px -10px rgba(0, 0, 0, 0.75)",
+      },
       height: {
         calcVH: "calc(100vh - 50px)",
       },
       flex: {
         4: "4 4 0%",
+        2: "2 2 0%",
       },
     },
   },
